cbb769d363d619c5226d5ec252f96795077b89af
B3xis <atatanishe@gmail.com>
2020-09-02 14:50:00 +0300

    Tesdt
    Yay

---
+++ Ermolaev/TestNaDebila.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ermolaev
+{
+    /// <summary>
+    /// Interaction logic for TestNaDebila.xaml
+    /// </summary>
+    public partial class TestNaDebila : Window
+    {
+        public TestNaDebila()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstYes_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Molodec");
+        }
+
+        private void FirstNo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ne Molodec");
+        }
+
+        private void FirstNox2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Molodec");
+        }
+    }
+}
